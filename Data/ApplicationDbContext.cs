09312d283593e2884558518d0054166101397e1e
Terogene Claudio <cterogene@gmail.com>
2022-01-24 10:38:38 -0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class ApplicationDbContext	:DbContext		
+
+{
+	public ApplicationDbContext()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
